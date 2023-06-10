1679d56a5e7499b7445c181cc2488c6b2baccb77
marcodestefano <marco.destefano1@stud.unifi.it>
2023-06-10 15:45:44 +0200

    Deploy Railway

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,8 +1,9 @@
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls.static import static
 import os
+from django.views.static import serve
 
 print(os.path.abspath(__file__))
 
@@ -14,6 +15,8 @@ urlpatterns = [
     path("workouts/", include("workouts.urls")),
     path("obiettivi/", include("obiettivi.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
+    re_path(r'^media/(?P<path>.)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
